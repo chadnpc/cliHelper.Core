bee00f18ce3df8a65c6cf521f24482fd90fcb6ea
Alain Herve <alainQtec@gmail.com>
2024-11-17 22:08:08 +0200

    Update Invoke-Ping.ps1

--- Public/Network/Invoke-Ping.ps1
+++ Public/Network/Invoke-Ping.ps1
@@ -1,5 +1,5 @@
 ﻿function Invoke-Ping {
-    <#
+  <#
     .SYNOPSIS
         Tests a connection to a host.
     .PARAMETER Name
@@ -11,23 +11,23 @@
     .EXAMPLE
         Invoke-Ping -name www.google.com -count 2 -ipv6
     #>
-    [cmdletbinding()]
-    param(
-        [parameter(Mandatory, Position = 0)]
-        [string]$Name,
+  [cmdletbinding()]
+  param(
+    [parameter(Mandatory, Position = 0)]
+    [string]$Name,
 
-        [parameter(Position = 1)]
-        [int]$Count = 5,
+    [parameter(Position = 1)]
+    [int]$Count = 5,
 
-        [parameter(position = 2)]
-        [switch]$IPv6
-    )
+    [parameter(position = 2)]
+    [switch]$IPv6
+  )
 
-    if ($PSBoundParameters.ContainsKey('IPv6')) {
-        $r = Invoke-Command -ScriptBlock { ping.exe $Name -n $Count -6 -a }
-    } else {
-        $r = Invoke-Command -ScriptBlock { ping.exe $Name -n $Count -4 -a }
-    }
+  if ($PSBoundParameters.ContainsKey('IPv6')) {
+    $r = Invoke-Command -ScriptBlock { ping.exe $Name -n $Count -6 -a }
+  } else {
+    $r = Invoke-Command -ScriptBlock { ping.exe $Name -n $Count -4 -a }
+  }
 
-    return ($r -Join "`n")
+  return ($r -Join "`n")
 }
